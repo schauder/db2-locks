fb67626a128368a82b177d1c37cbb0d04b19ff9b
Jens Schauder <jens@schauderhaft.de>
2020-05-08 11:44:03 +0200

    polishing test and running it 10 time to make sure we are not observing a fluke.

--- src/test/java/de/schauderhaft/db2locks/Db2locksApplicationTests.java
+++ src/test/java/de/schauderhaft/db2locks/Db2locksApplicationTests.java
@@ -1,16 +1,11 @@
 package de.schauderhaft.db2locks;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
@@ -22,6 +17,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import static java.util.Collections.*;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -35,56 +31,52 @@ class Db2locksApplicationTests {
 	@Autowired
 	TransactionTemplate tx;
 
-	@Autowired
-	DataSource dataSource;
-
 	CountDownLatch firstQueryExecuted = new CountDownLatch(1);
 	CountDownLatch firstTransactionFinished = new CountDownLatch(1);
 	CountDownLatch secondTransactionFinished = new CountDownLatch(1);
 
+	@BeforeEach
+void before() {
+		template.update("delete from example", emptyMap());
+	}
 	@Test
 	void contextLoads() {
 
-		String result = template.queryForObject("SELECT 'Alfred' AS NAME FROM SYSIBM.SYSDUMMY1", Collections.emptyMap(), String.class);
+		String result = template.queryForObject("SELECT 'Alfred' AS NAME FROM SYSIBM.SYSDUMMY1", emptyMap(), String.class);
 
 		assertThat(result).isEqualTo("Alfred");
 
 	}
 
-	@Test
+	@RepeatedTest(10)
 	void orchestratedUpdate() throws InterruptedException {
 
 		template.update("insert into example (id, text) values (:id, :text)", parameters(23, "Insert"));
 
-		new Thread(() -> {
-			tx.executeWithoutResult(tx -> {
-				try {
-					updateAndWait("First Update");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			});
-		}).start();
+		runInThread(() -> updateAndWait("First Update"));
 
-		new Thread(() -> {
-			tx.executeWithoutResult(tx -> {
-				try {
-					waitAndUpdate("Second Update");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			});
-		}).start();
+		runInThread(() -> waitAndUpdate("Second Update"));
 
 		firstQueryExecuted.await(100, TimeUnit.MILLISECONDS);
 		log.add("wait");
 		Thread.sleep(100);
 		firstTransactionFinished.countDown();
+		secondTransactionFinished.await(100, TimeUnit.MILLISECONDS);
 
 		assertThat(log).containsExactly("First Update", "wait", "Second Update");
 	}
 
+	private void runInThread(SafeRunnable runnable) {
+
+		new Thread(() -> {
+			tx.executeWithoutResult(tx -> {
+					runnable.run();
+			});
+		}).start();
+	}
+
 	private void updateAndWait(String text) throws InterruptedException {
+
 		template.update("update example set text = :text where id = :id", parameters(23, text));
 		log.add(text);
 		firstQueryExecuted.countDown();
